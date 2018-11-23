b17c180f05d14438f627fc8c9c201b819ba9ba1b
Vadym Rusin <vadimrusin94@gmail.com>
2018-11-23 21:51:46 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="app-phone">
+      <div class="phone-header">
+
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png" />
+
+        <a class="cancel-cta" v-if="step === 2 || step === 3" @click="goToHome">
+          Cancel
+        </a>
+
+        <a class="next-cta" v-if="step === 2" @click="step++">
+          Next
+        </a>
+      </div>
+
+      <phone-body :posts="posts" :filters="filters" :step="step" :image="image" :selectedFilter="selectedFilter"
+        v-model="caption"/>
+
+      <div class="phone-footer">
+        <div class="home-cta" @click="goToHome">
+          <i class="fas fa-home fa-lg"></i>
+        </div>
+
+        <div class="upload-cta">
+          <input type="file" name="file" id="file" class="inputfile" @change="uploadImage" :disabled="step !== 1" />
+          <label for="file">
+            <i class="far fa-plus-square fa-lg"></i>
+          </label>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PhoneBody from "./components/PhoneBody";
+import posts from "./data/posts";
+import filters from "./data/filters";
+import EventBus from "./event-bus.js";
 export default {
-  name: 'app',
+  name: "App",
+  data() {
+    return {
+      step: 1,
+      posts,
+      filters,
+      image: "",
+      selectedFilter: "",
+      caption: ""
+    };
+  },
   components: {
-    HelloWorld
+    "phone-body": PhoneBody
+  },
+  methods: {
+    uploadImage(event) {
+      const files = event.target.files;
+      if (!files.length) return;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = event => {
+        this.image = event.target.result;
+        this.step = 2;
+      };
+    },
+    goToHome() {
+      this.image = "";
+      this.selectedFilter = "";
+      this.caption = "";
+      this.step = 1;
+    }
+  },
+  created() {
+    EventBus.$on("filter-selected", event => {
+      this.selectedFilter = event.filter;
+    });
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" src="./styles/app.scss">
+// Styles from stylesheet
 </style>
---
+++ src/components/PhoneBody.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="phone-body">
+
+    <div class="feed" v-show="step === 1">
+      <vuegram-post v-for="post in posts" :post="post" :key="posts.indexOf(post)" />
+    </div>
+
+    <div v-show="step === 2">
+      <div class="selected-image" :class="selectedFilter" :style="{backgroundImage: 'url(' + image + ')' }">
+        <div class="filter-container">
+
+        </div>
+      </div>
+    </div>
+
+    <div v-show="step === 3">
+      <div class="selected-image" :class="selectedFilter" :style="{ backgroundImage: 'url(' + image + ')' }">
+      </div>
+      <div class="caption-container">
+        <textarea class="caption-input" placeholder="Write a caption..." type="text">
+        </textarea>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import VuegramPost from "./VuegramPost";
+export default {
+  name: "PhoneBody",
+  props: {
+    posts: Array,
+    filters: Array,
+    step: Number,
+    image: String,
+    selectedFilter: String
+  },
+  components: {
+    "vuegram-post": VuegramPost
+  }
+};
+</script>
+
+<style lang="scss" src="../styles/phone-body.scss">
+// Styles from stylesheet
+</style>
